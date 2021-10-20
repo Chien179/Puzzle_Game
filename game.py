43d921caf03ac8e79581e25dad4dc501ea74b3ff
phianh5@gmail.com <phianh5@gmail.com>
2021-10-20 13:13:44 +0700

    add background music

--- game.py
+++ game.py
@@ -1,7 +1,6 @@
 import pygame
 from puzzle import Puzzle
 
-
 class Game:
     def __init__(self, width, height, imgWidth, imgHeight, size):
         pygame.init()
@@ -14,8 +13,9 @@ class Game:
         self.__start = False
         pygame.display.set_caption('Puzzle')
 
-    def __create_font(self, font, fontSize, content, color):
-        f = pygame.font.SysFont(font, fontSize, False, False)
+
+    def __create_font(self, font, fontSize, content, color, isBold):
+        f = pygame.font.SysFont(font, fontSize, isBold, False)
         text = f.render(content, False, color)
         return text
 
@@ -25,14 +25,19 @@ class Game:
         surface.fill(self.__backgroundColor)
         
         # text
-        shuffle_text = self.__create_font(font='Arial', fontSize=30, content='Shuffle', color='#FFFFFF')
+        shuffle_text = self.__create_font(font='Arial', fontSize=30, content='Shuffle', color='#FFFFFF', isBold=False)
         self.__shuffle_text_rect = shuffle_text.get_rect(midleft=(650, 350))
 
-        title_text = self.__create_font(font='Arial', fontSize=35, content='PUZZLE', color='#FFFFFF')
+        title_text = self.__create_font(font='Times New Roman', fontSize=35, content='PUZZLE', color='#FFFFFF', isBold=True)
         self.__title_text_rect = title_text.get_rect(center=(self.__width/2, 50))
 
         surface.blit(title_text, self.__title_text_rect)
         surface.blit(shuffle_text, self.__shuffle_text_rect)
+
+
+        music_text = self.__create_font(font='Arial', fontSize=20, content='MUTE', color='#FFFFFF', isBold=False)
+        self.__music_text_rect = music_text.get_rect(midleft=(20, 20))
+        surface.blit(music_text, self.__music_text_rect)
 
     def __drawPuzzle(self):
         x, y = 20, 100
@@ -66,8 +71,22 @@ class Game:
                             if self.__puzzle.imgRect[i][j].collidepoint(pygame.mouse.get_pos()):
                                 self.__puzzle.control(i, j)
                             self.__drawPuzzle()
+                elif self.__music_text_rect.collidepoint(pygame.mouse.get_pos()):
+                    if self.__isPlayingMusic == True:
+                        pygame.mixer.music.pause()
+                        self.__isPlayingMusic = False
+                    else:
+                        pygame.mixer.music.unpause()
+                        self.__isPlayingMusic = True
+
+    def __playBackgroundMusic(self):
+        self.__isPlayingMusic = True
+        pygame.mixer.music.unload()
+        pygame.mixer.music.load("sounds/bgmusic1.mp3")
+        pygame.mixer.music.play(loops=0, start=0.0)
 
     def game_loop(self):
+        self.__playBackgroundMusic()
         while True:
             self.__update()
             self.__draw()
