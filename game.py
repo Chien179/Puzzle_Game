33282bf13210e32d0d850dea9575926f043d4729
chien <chiennnm456@gmail.com>
2021-10-20 16:33:39 +0700

    add some effect display

--- game.py
+++ game.py
@@ -1,15 +1,15 @@
 import pygame
+import pygame.gfxdraw
 from puzzle import Puzzle
 
 class Game:
     def __init__(self, width, height, imgWidth, imgHeight, size):
         pygame.init()
-        self.__backgroundColor = '#8AAAE5'
         self.__width, self.__height = width, height
         self.__imgWidth, self.__imgHeight = imgWidth, imgHeight
         self.__size = size
         self.__screen = pygame.display.set_mode((self.__width,self.__height))
-        self.__puzzle = Puzzle('images/1.jpg', self.__backgroundColor, self.__imgWidth, self.__imgHeight, self.__size)
+        self.__puzzle = Puzzle('images/1.jpg', self.__imgWidth, self.__imgHeight, self.__size)
         self.__start = False
         pygame.display.set_caption('Puzzle')
 
@@ -22,7 +22,7 @@ class Game:
     def __draw(self):
         # white screen
         surface = self.__screen
-        surface.fill(self.__backgroundColor)
+        surface.fill('#8AAAE5')
         
         # text
         shuffle_text = self.__create_font(font='Arial', fontSize=30, content='Shuffle', color='#FFFFFF', isBold=False)
@@ -31,26 +31,35 @@ class Game:
         title_text = self.__create_font(font='Times New Roman', fontSize=35, content='PUZZLE', color='#FFFFFF', isBold=True)
         self.__title_text_rect = title_text.get_rect(center=(self.__width/2, 50))
 
-        surface.blit(title_text, self.__title_text_rect)
-        surface.blit(shuffle_text, self.__shuffle_text_rect)
-
-
         music_text = self.__create_font(font='Arial', fontSize=20, content='MUTE', color='#FFFFFF', isBold=False)
         self.__music_text_rect = music_text.get_rect(midleft=(20, 20))
+
+        surface.blit(title_text, self.__title_text_rect)
+        surface.blit(shuffle_text, self.__shuffle_text_rect)
         surface.blit(music_text, self.__music_text_rect)
+        if self.__start:
+            self.__screen.fill((0, 0, 0))
+        else:
+            surface.blit(self.__puzzle.image, (20, 100))
+
 
     def __drawPuzzle(self):
+        self.__puzzle.image.set_alpha(200)
+        self.__screen.blit(self.__puzzle.image, (20, 100))
+
+        imgPieceWidth = self.__imgWidth / 3
         x, y = 20, 100
-        for i in range(0, 3):
+        for i in range(0, self.__size):
             imgTemp = []
-            for j in range(0, 3):
+            for j in range(0, self.__size):
                 # creat rect to move image pieces
                 imgTemp.append(self.__puzzle.imgPieces[i][j].get_rect(left=x, top=y))
+                pygame.draw.rect(self.__puzzle.imgPieces[i][j], (0, 0, 0), pygame.Rect(0, 0, imgPieceWidth + 1, imgPieceWidth + 1), 1)
                 self.__screen.blit(self.__puzzle.imgPieces[i][j], imgTemp[j])
-                x += 480 / 3
-                if j == 2:
+                x += imgPieceWidth
+                if j == self.__size - 1:
                     x = 20
-                    y += 480 / 3
+                    y += imgPieceWidth
             self.__puzzle.imgRect.append(imgTemp)
 
     def __update(self):
@@ -61,18 +70,18 @@ class Game:
             if event.type == pygame.MOUSEBUTTONUP:
                 if self.__shuffle_text_rect.collidepoint(pygame.mouse.get_pos()):
                     self.__puzzle.imgPieces.clear()
-                    self.__puzzle = Puzzle('images/1.jpg', self.__backgroundColor, self.__imgWidth, self.__imgHeight, self.__size)
+                    self.__puzzle = Puzzle('images/1.jpg', self.__imgWidth, self.__imgHeight, self.__size)
                     self.__puzzle.shuffle()
                     self.__drawPuzzle()
                     self.__start = True
-                elif self.__start is True:
+                elif self.__start:
                     for i in range(0, self.__size):
                         for j in range(0, self.__size):
                             if self.__puzzle.imgRect[i][j].collidepoint(pygame.mouse.get_pos()):
                                 self.__puzzle.control(i, j)
                             self.__drawPuzzle()
-                elif self.__music_text_rect.collidepoint(pygame.mouse.get_pos()):
-                    if self.__isPlayingMusic == True:
+                if self.__music_text_rect.collidepoint(pygame.mouse.get_pos()):
+                    if self.__isPlayingMusic:
                         pygame.mixer.music.pause()
                         self.__isPlayingMusic = False
                     else:
@@ -87,9 +96,8 @@ class Game:
 
     def game_loop(self):
         self.__playBackgroundMusic()
+        self.__draw()
         while True:
             self.__update()
-            self.__draw()
-            self.__drawPuzzle()
             pygame.display.update()
             pygame.time.Clock().tick(60)
