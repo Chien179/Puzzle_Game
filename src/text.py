6b48b122cf538bf7a070b6b63eb8a6a19a99b0b6
chien <chiennnm456@gmail.com>
2021-11-04 08:21:24 +0700

    event win

---
+++ src/text.py
@@ -0,0 +1,36 @@
+import pygame
+
+class Text:
+    def __init__(self, font, fontSize, content, color, isBold = False, isSys = False):
+        self.__font = font
+        self.__fontSize = fontSize
+        self.__content = content
+        self.__color = color
+        self.__isBold = isBold
+        self.__isSys = isSys
+        if self.__isSys:
+            self.text = self.__create_Sysfont()
+        else:
+            self.text = self.__create_Font()
+
+    def __create_Sysfont(self):
+        f = pygame.font.SysFont(self.__font, self.__fontSize, self.__isBold, False)
+        text = f.render(self.__content, True, self.__color)
+        return text
+
+    def __create_Font(self):
+        f = pygame.font.Font(self.__font, self.__fontSize)
+        text = f.render(self.__content, True, self.__color)
+        return text
+
+    def set_color_hovered(self, color):
+        self.__color = color #hovered
+
+    def set_color_left(self, color):
+        self.__color = color #mouse leave
+
+    def update(self):
+        if self.__isSys:
+            self.text = self.__create_Sysfont()
+        else:
+            self.text = self.__create_Font()
